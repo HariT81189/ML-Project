2d12441c39973492aff3f7d371d1045c53612eb1
HariT81189 <hari.turangi@gmail.com>
2026-07-27 23:33:56 +0530

    Hyperparameter Trained

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -16,6 +16,7 @@ from xgboost import XGBRegressor
 
 from src.exception import CustomException
 from src.logger import logging
+
 from src.utils import save_object,evaluate_models
 
 @dataclass
@@ -34,7 +35,7 @@ class ModelTrainer:
                 train_array[:,:-1],
                 train_array[:,-1],
                 test_array[:,:-1],
-                test_array[:,-1],
+                test_array[:,-1]
             )
             models = {
                 "Random Forest": RandomForestRegressor(),
@@ -43,25 +44,62 @@ class ModelTrainer:
                 "Linear Regression": LinearRegression(),
                 "XGBRegressor": XGBRegressor(),
                 "CatBoosting Regressor": CatBoostRegressor(verbose=False),
-                "AdaBoost Regressor": AdaBoostRegressor()
+                "AdaBoost Regressor": AdaBoostRegressor(),
+            }
+            params={
+                "Decision Tree": {
+                    'criterion':['squared_error', 'absolute_error', 'poisson'],
+                    # 'splitter':['best','random'],
+                    # 'max_features':['sqrt','log2'],
+                },
+                "Random Forest":{
+                    # 'criterion':['squared_error', 'absolute_error', 'poisson'],
+                 
+                    # 'max_features':['sqrt','log2',None],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Gradient Boosting":{
+                    # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    # 'criterion':['squared_error', 'friedman_mse'],
+                    # 'max_features':['auto','sqrt','log2'],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Linear Regression":{},
+                "XGBRegressor":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "CatBoosting Regressor":{
+                    'depth': [6,8,10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100]
+                },
+                "AdaBoost Regressor":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    # 'loss':['linear','square','exponential'],
+                    'n_estimators': [8,16,32,64,128,256]
+                }
+                
             }
 
-            model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,
-                                             X_test=X_test,y_test=y_test,models=models)
+            model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,
+                                             models=models,param=params)
+            
+            ## To get best model score from dict
+            best_model_score = max(sorted(model_report.values()))
 
+            ## To get best model name from dict
 
-            # To Get Best Model Score from Dict
-            best_model_score=max(sorted(model_report.values()))
-
-            # To Get best Model Name from Dict
-            best_model_name=list(model_report.keys())[
+            best_model_name = list(model_report.keys())[
                 list(model_report.values()).index(best_model_score)
             ]
-
             best_model = models[best_model_name]
+
             if best_model_score<0.6:
-                raise CustomException("No Best Model Found")
-            logging.info(f'Best Found Model on Both Traing and test Dataset')
+                raise CustomException("No best model found")
+            logging.info(f"Best found model on both training and testing dataset")
 
             save_object(
                 file_path=self.model_trainer_config.trained_model_file_path,
@@ -70,10 +108,12 @@ class ModelTrainer:
 
             predicted=best_model.predict(X_test)
 
-            score = r2_score(y_test,predicted)
-            return score
+            r2_square = r2_score(y_test, predicted)
+            return r2_square
+            
 
 
+
+            
         except Exception as e:
             raise CustomException(e,sys)
-            
--- src/utils.py
+++ src/utils.py
@@ -20,14 +20,28 @@ def save_object(file_path,obj):
     except Exception as e:
         raise CustomException(e,sys)
 
-def evaluate_models(X_train,y_train,X_test,y_test,models):
-    try:
+def evaluate_models(X_train,y_train,X_test,y_test,models,param,cv=3):
+    try: 
         report={}
 
         for i in range(len(list(models))):
-            model = list(models.values())[i]
+            model=list(models.values())[i]
+            model_name = list(models.keys())[i]
+            para = param[model_name]
 
-            model.fit(X_train,y_train) # Train Model
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
+
+            
+    
+            
+            
+            #model.fit(X_train,y_train)
+
+            #model.fit(X_train,y_train) # Train Model
 
             y_train_pred = model.predict(X_train)
             y_test_pred = model.predict(X_test)
